e023df4b060babd600c548a9f85e469322865cce
Anna Warzecha <anna.warzecha@gmail.com>
2018-08-18 10:37:45 +0200

    Add postgresql db settings

--- vintourage/database.py
+++ vintourage/database.py
@@ -1,8 +1,11 @@
+import os
+
 from sqlalchemy import create_engine
 from sqlalchemy.orm import scoped_session, sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
-engine = create_engine('sqlite:///simple.db', convert_unicode=True)
+DATABASE_URL = os.environ.get('DATABASE_URL', 'sqlite:///simple.db')
+engine = create_engine(DATABASE_URL, convert_unicode=True)
 db_session = scoped_session(sessionmaker(autocommit=False,
                                          autoflush=False,
                                          bind=engine))
