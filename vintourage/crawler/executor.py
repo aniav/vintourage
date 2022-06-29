12ec3fc3f98c8ae56120bc5a9f53b50f10ab86e1
Ania Warzecha <anna.warzecha@gmail.com>
2022-06-29 23:11:02 +0200

    Remove klunken.pl which no longer exists

--- vintourage/crawler/executor.py
+++ vintourage/crawler/executor.py
@@ -4,13 +4,12 @@ from scrapy.utils.project import get_project_settings
 
 from spiders.dewitched import DewitchedSpider
 from spiders.inspired import InspiredSpider
-from spiders.klunken import KlunkenSpider
 from spiders.ragsandsilks import RagsandsilksSpider
 from spiders.somavintage import SomavintageSpider
 from spiders.vintageladies import VintageladiesSpider
 
 crawlers = [
-    DewitchedSpider, InspiredSpider, KlunkenSpider, RagsandsilksSpider,
+    DewitchedSpider, InspiredSpider, RagsandsilksSpider,
     SomavintageSpider, VintageladiesSpider
 ]
 process = CrawlerProcess(get_project_settings())
