c468be91cd6b1aa47f6705fe33a6ecaf8aa007a2
Anna Warzecha <anna.warzecha@gmail.com>
2023-01-14 23:07:11 +0100

    Remove dewitched as it no longer exists

--- vintourage/crawler/executor.py
+++ vintourage/crawler/executor.py
@@ -1,15 +1,13 @@
-import scrapy
 from scrapy.crawler import CrawlerProcess
 from scrapy.utils.project import get_project_settings
 
-from spiders.dewitched import DewitchedSpider
 from spiders.inspired import InspiredSpider
 from spiders.ragsandsilks import RagsandsilksSpider
 from spiders.somavintage import SomavintageSpider
 from spiders.vintageladies import VintageladiesSpider
 
 crawlers = [
-    DewitchedSpider, InspiredSpider, RagsandsilksSpider,
+    InspiredSpider, RagsandsilksSpider,
     SomavintageSpider, VintageladiesSpider
 ]
 process = CrawlerProcess(get_project_settings())
